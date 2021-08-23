efb48ea68fe7b69d2e011feea7c863f693db95c0
themhz <themhz@gmail.com>
2021-08-23 04:31:16 +0300

    Add project files.

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Name { get; set; }
+
+        public int Author { get; set; }
+
+        public String ISBN { get; set; }
+    }
+}
---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            this._db = db;
+        }       
+        
+        [BindProperty]
+        public Book Book { get; set; }
+        
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
